44abd30bd631afa8f74aa719e591f93e75658ac2
Jin Cheng <jincheng88@outlook.com>
2017-09-26 03:00:13 -0400

    added addToCart controller

---
+++ src/main/java/com/bookstore/repository/BookToCartItemRepository.java
@@ -0,0 +1,10 @@
+package com.bookstore.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bookstore.domain.BookToCartItem;
+
+
+public interface BookToCartItemRepository extends CrudRepository<BookToCartItem, Long>{
+
+}
