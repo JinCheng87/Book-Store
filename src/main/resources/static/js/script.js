ab7549ff83179635fcf73206c88a41ee08563321
Jin Cheng <jincheng88@outlook.com>
2017-10-01 10:48:21 -0400

    set billing address same as shipping address

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,11 +1,20 @@
 /**
  * 
  */
+function checkBillingAddress() {
+	if ($("#theSameAsShippingAddress").is(":checked")) {
+		$(".billingAddress").prop("disabled", true);
+	} else {
+		$(".billingAddress").prop("disabled", false);
+	}
+}
 
-$(document).ready(function(){
-	$(".cartItemQty").on('change', function(){
-		var id=this.id;
+$(document).ready(function() {
+	$(".cartItemQty").on('change', function() {
+		var id = this.id;
 		console.log('111');
-		$('#update-item-'+id).css('display','inline-block');
+		$('#update-item-' + id).css('display', 'inline-block');
 	})
+	
+	$("#theSameAsShippingAddress").on('click', checkBillingAddress);
 })
