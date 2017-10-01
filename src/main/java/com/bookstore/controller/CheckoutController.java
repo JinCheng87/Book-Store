e8ff72a6aead5a654f46971bc0e894f96ca7d8cc
Jin Cheng <jincheng88@outlook.com>
2017-10-01 17:14:58 -0400

    added confirmation email template

--- src/main/java/com/bookstore/controller/CheckoutController.java
+++ src/main/java/com/bookstore/controller/CheckoutController.java
@@ -34,6 +34,7 @@ import com.bookstore.service.ShoppingCartService;
 import com.bookstore.service.UserPaymentService;
 import com.bookstore.service.UserService;
 import com.bookstore.service.UserShippingService;
+import com.bookstore.utility.MailConstructor;
 import com.bookstore.utility.USConstants;
 
 @Controller
@@ -71,6 +72,9 @@ public class CheckoutController {
 	
 	@Autowired
 	private JavaMailSender mailSender;
+	
+	@Autowired
+	private MailConstructor mailConstructor;
 
 	@RequestMapping("/checkout")
 	public String checkout(@RequestParam("id") Long cartId,
